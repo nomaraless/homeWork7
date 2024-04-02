af9c3559919430ef29da3e2eca46d7e6f22c8f7d
nomar <nomaraless@gmail.com>
2024-04-02 18:00:51 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        double depAmount = 15000;
+        double sav = 0;
+        int i=1;
+        while (sav <= 2_459_000) {
+            sav = sav + sav/100;
+            sav = sav + depAmount;
+            i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + String.format("%.2f", sav) + " рублей");
+        }
     }
 }
